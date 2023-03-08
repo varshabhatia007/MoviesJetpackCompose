995d1624343c9b50d9c58df4ae61e1dc2701653b
varsha <vbhatia_2009@ymail.com>
2023-03-08 10:10:18 +0000

    Add API service for movies including top rated movies, popular movies, upcoming movies etc

--- app/src/main/java/com/example/moviesjetpackcompose/data/ApiService.kt
+++ app/src/main/java/com/example/moviesjetpackcompose/data/ApiService.kt
@@ -14,4 +14,31 @@ interface ApiService {
         @Query("language") language: String = "en"
     ): FilmResponse
 
+    @GET("movie/popular")
+    suspend fun getPopularMovies(
+        @Query("page") page: Int = 0,
+        @Query("api_key") apiKey: String = BuildConfig.MOVIE_API_KEY,
+        @Query("language") language: String = "en"
+    ): FilmResponse
+
+    @GET("movie/top_rated")
+    suspend fun getTopRatedMovies(
+        @Query("page") page: Int = 0,
+        @Query("api_key") apiKey: String = BuildConfig.MOVIE_API_KEY,
+        @Query("language") language: String = "en"
+    ): FilmResponse
+
+    @GET("movie/now_playing")
+    suspend fun getNowPlayingMovies(
+        @Query("page") page: Int = 0,
+        @Query("api_key") apiKey: String = BuildConfig.MOVIE_API_KEY,
+        @Query("language") language: String = "en"
+    ): FilmResponse
+
+    @GET("movie/upcoming")
+    suspend fun getUpcomingMovies(
+        @Query("page") page: Int = 0,
+        @Query("api_key") apiKey: String = BuildConfig.MOVIE_API_KEY,
+        @Query("language") language: String = "en"
+    ): FilmResponse
 }
